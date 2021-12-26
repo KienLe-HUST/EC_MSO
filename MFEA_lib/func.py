0a1710b142b2d715d2caf0c0f63cd16c65135437
KienLe-git <kien.letrung610@gmail.com>
2021-12-26 21:23:14 +0700

    func

--- MFEA_lib/func.py
+++ MFEA_lib/func.py
@@ -1,30 +1,29 @@
 import numpy as np
 
-class fnc:
+class AbstractFunc():
     limited_space = False
     upper_bound = None
     lower_bound = None
     name = ''
 
-    def __init__(self, d, shift = 0, rotation_matrix: np.ndarray = None,
+    def __init__(self, dim, shift = 0, rotation_matrix: np.ndarray = None, 
                 limited_space: bool = False, lower_bound = None, upper_bound = None):
-        self.d = d
-        self.name = self.__class__.__name__ + "_d:"+ str(d) + "_translation:" + str(shift)
+        self.dim = dim
+        #NOTE
+        self.name = self.__class__.name
+
         if rotation_matrix is not None:
-            assert np.all(np.array(rotation_matrix.shape) == d)
+            assert np.all(np.array(rotation_matrix.shape) == dim)
             self.rotation_matrix = rotation_matrix
-            self.inv_ro_matrix = np.linalg.inv(self.rotation_matrix)
+            self.inv_rotation_matrix = np.linalg.inv(self.rotation_matrix)
         else:
-            self.rotation_matrix = np.identity(d)
-            self.inv_ro_matrix = np.identity(d)
-
-
-        if type(shift) != list:
-            self.shift = shift
-        else:
-            assert d % len(shift) == 0
-            self.shift = np.array([[i] * int(d / len(shift)) for i in shift ]).reshape(-1, )
+            self.rotation_matrix = np.identity(dim)
+            self.inv_rotation_matrix = np.identity(dim)
         
+        tmp = np.array(shift)
+        assert dim % len(shift) == 0
+        self.shift = np.array([[i] * int(dim / len(tmp)) for i in tmp ]).reshape(-1, )
+
         if limited_space == True:
             if lower_bound is not None and upper_bound is not None:
                 self.limited_space = limited_space
@@ -34,40 +33,37 @@ class fnc:
                 raise Exception("Limitted spacce must have both inf and sup val")
 
     def encode(self, x):
+        '''
+        encode x to [0, 1]
+        '''
         x_encode = x
-        x_encode = self.inv_ro_matrix @ x_encode + self.shift
-        
+        x_encode = self.inv_rotation_matrix @ x_encode + self.shift
         if self.limited_space == True:
             x_encode = (x_encode - self.lower_bound)/(self.upper_bound - self.lower_bound)
-        return x_encode
-
-# sphere: 0 ->shift = 80, matrix = M;      
-# encode:
-#   input x   
-#   x = invM*x + shift
-#   scale x
-# decode:
-#   scale x
-#   x = M * (x- shift)
+        return x_encode 
 
     def decode(self, x):
-        x_decoded = x[:self.d]
+        '''
+        decode x
+        '''
+        x_decode = x[:self.dim]
         if self.limited_space == True:
-            x_decoded = x_decoded * (self.upper_bound - self.lower_bound) + self.lower_bound
+            x_decode = x_decode * (self.upper_bound - self.lower_bound) + self.lower_bound
+        x_decode = self.rotation_matrix @ (x_decode - self.shift) 
+        return x_decode
 
-        x_decoded = self.rotation_matrix @ (x_decoded - self.shift) 
-        return x_decoded
-
-class sphere(fnc):
+class Sphere(AbstractFunc):
     '''
     global optima = 0^d
     '''
     def func(self, x):
+        '''
+        Request: input x is encoded
+        '''
         x = self.decode(x)
-        
         return np.sum(x**2, axis = 0)
 
-class weierstrass(fnc):
+class Weierstrass(AbstractFunc):
     '''
     global optima = 0^d
     '''
@@ -75,8 +71,10 @@ class weierstrass(fnc):
     b = 3
     k_max = 21
     def func(self, x):
+        '''
+        Request: input x is encoded
+        '''
         x = self.decode(x)
-
         left = 0
         for i in range(self.d):
             left += np.sum(self.a ** np.arange(self.k_max) * \
@@ -87,58 +85,53 @@ class weierstrass(fnc):
         )
         return left - right
 
-class ackley(fnc):
+class Ackley(AbstractFunc):
     '''
     global optima = 0^d
     '''
     a = 20
     b = 0.2
     c = 2*np.pi 
-        
     def func(self, x):
         x = self.decode(x)
-        
-        return (-self.a * np.exp(-self.b*np.sqrt(np.average(x**2)))
-        - np.exp(np.average(np.cos(self.c * x)))
-        + self.a
+        return -self.a * np.exp(-self.b*np.sqrt(np.average(x**2)))\
+        - np.exp(np.average(np.cos(self.c * x)))\
+        + self.a\
         + np.exp(1)
-        )
-class rosenbrock(fnc):
-    '''
-    global optima = 1^d
-    '''
 
+class Rosenbrock(AbstractFunc):
+    '''
+    global optima = 0^d
+    '''
     def func(self, x):
-        x = self.decode(x)
-        
+        x = self.decode(x) + 1
         l = 100*np.sum((np.delete(x, 0, 0) - np.delete(x, -1, 0 )**2) ** 2)
         r = np.sum((np.delete(x, -1, 0) - 1) ** 2)
         return l + r
-class schwefel(fnc):
-    '''
-    global optima = [420.9687]^d
-    '''
 
+class Schwefel(AbstractFunc):
+    '''
+    global optima = 0^d
+    '''
     def func(self, x):
-        x = self.decode(x)
-        
+        x = self.decode(x) + 420.9687
         return 418.9829*self.d - np.sum(x * np.sin(np.sqrt(np.abs(x))))
-class griewank(fnc):
+    
+class Griewank(AbstractFunc):
     ''' 
     global optima = [0] ^ d
     '''
-
     def func(self, x):
         x = self.decode(x)
-
         return np.sum(x**2) / 4000 \
-            - np.prod(np.cos(x / np.sqrt((np.arange(self.d) + 1)))) + 1        
-class rastrigin(fnc):
+            - np.prod(np.cos(x / np.sqrt((np.arange(self.d) + 1)))) + 1
+
+class Rastrigin(AbstractFunc):
     ''' 
     global optima = 0 ^ d
     '''
-
     def func(self, x):
         x = self.decode(x)
         
         return 10 * self.d + np.sum(x ** 2 - 10 * np.cos(2 * np.pi * x))
+
---
+++ MFEA_lib/model.py
@@ -0,0 +1,15 @@
+from .GA import population_init, factorial_cost, factorial_rank, skill_factor_best_task, polynomial_mutation, sbx_crossover
+
+class AbstractModel():
+    def __init__(self, num_epochs, num_inds_each_task, range_init_pop = [0, 1],  
+                one_line = False, num_epochs_printed = 20, evaluate_initial_skillFactor = True) -> None:
+        self.num_epochs = num_epochs
+        self.num_inds_each_task = num_inds_each_task
+        self.range_init_pop = range_init_pop
+        self.one_line = one_line
+        self.num_epochs_printed = num_epochs_printed
+        self.evaluate_initial_skillFactor = evaluate_initial_skillFactor
+    def compile(self):
+        pass
+    def fit(self, tasks = []):
+        pass
