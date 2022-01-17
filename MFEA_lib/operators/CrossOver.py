1ee1977d39662d67d64263462debc83c84e2ff9f
KienLe-git <kien.letrung610@gmail.com>
2022-01-17 22:13:58 +0700

    mutation swap in crossover

--- MFEA_lib/operators/CrossOver.py
+++ MFEA_lib/operators/CrossOver.py
@@ -73,7 +73,16 @@ class SBX_refuseInter(AbstractCrossOver):
             c1[idx], c2[idx] = c2[idx], c1[idx]
             
         elif type == 'inter1skf':
-            idx_refuse = np.where((np.random.rand(len(pa)) < p_refuse_inter) * (np.abs(c2 - c1) > d_swap))[0]
-            c2[idx_refuse] = c1[idx_refuse]
+            # idx_refuse = np.where((np.random.rand(len(pa)) < p_refuse_inter) * (np.abs(c2 - c1) > d_swap))[0]
+            # c2[idx_refuse] = c1[idx_refuse]
+
+            idx_swap = np.where((np.random.rand(len(pa)) < 0.5) * (np.abs(c2 - c1) < d_swap))[0]
+            c1[idx_swap], c2[idx_swap] = c2[idx_swap], c1[idx_swap]
+
+            # mutation
+            pm = 1/len(pa)
+            idx_mutation = np.where(np.random.rand(len(pa)) < pm)[0]
+            c1[idx_mutation] = pb[idx_mutation]
+
 
         return c1, c2
