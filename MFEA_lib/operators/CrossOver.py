17f6536af57b08fe88a46cebddb84c100a77bb32
KienLe-git <kien.letrung610@gmail.com>
2022-01-18 17:52:11 +0700

    newSBX crossover -> cec17 5tasks = 0

--- MFEA_lib/operators/CrossOver.py
+++ MFEA_lib/operators/CrossOver.py
@@ -1,3 +1,4 @@
+from re import U
 from typing import Tuple
 import numpy as np
 
@@ -44,3 +45,81 @@ class SBX_CrossOver(AbstractCrossOver):
 
         return c1, c2
 
+class newSBX(AbstractCrossOver):
+    '''
+    pa, pb in [0, 1]^n
+    '''
+    def __init__(self, nb_tasks: int, nc = 15, gamma = .9):
+        self.nc = nc
+        self.nb_tasks = nb_tasks
+        self.gamma = gamma
+
+    def get_dim_uss(self, dim_uss):
+        self.dim_uss = dim_uss
+        self.prob = np.ones((self.nb_tasks, self.nb_tasks, dim_uss))
+        self.sum_crossover_each_dimensions = np.zeros((self.nb_tasks, self.nb_tasks, dim_uss))
+        self.success_crossover_each_dimension = np.zeros((self.nb_tasks, self.nb_tasks, dim_uss))
+        self.epoch_idx_crossover = []
+        self.skf_parent = np.empty((0, 2), dtype= int)
+
+    def update(self, idx_success):
+        # sum success crossover
+        for idx in idx_success:
+            self.success_crossover_each_dimension[self.skf_parent[idx][0], self.skf_parent[idx][1]] += self.epoch_idx_crossover[idx]
+
+        # percent success:
+        per_success = np.copy(self.prob)
+        per_success = np.where(
+            self.sum_crossover_each_dimensions != 0, 
+            self.success_crossover_each_dimension / (self.success_crossover_each_dimension + 1e-10),
+            self.prob
+        )
+
+        # update prob 
+        self.prob = self.prob * self.gamma + (1 - self.gamma) * per_success
+
+        # reset
+        self.sum_crossover_each_dimensions = np.zeros((self.nb_tasks, self.nb_tasks, self.dim_uss))
+        self.success_crossover_each_dimension = np.zeros((self.nb_tasks, self.nb_tasks, self.dim_uss))
+        self.epoch_idx_crossover = []
+        self.skf_parent = np.empty((0, 2), dtype= int)
+
+        self.prob = np.clip(self.prob, 1/self.dim_uss, 1)
+        
+    def __call__(self, pa, pb, skf: tuple[int, int]) -> Tuple[np.ndarray, np.ndarray]:
+        '''
+        skf = (skf_pa, skf_pb)
+        '''
+        idx_crossover = (np.random.rand(self.dim_uss) < self.prob[skf[0], skf[1]]) 
+        self.sum_crossover_each_dimensions[skf[0], skf[1]] += 2 * idx_crossover
+        self.epoch_idx_crossover.append(idx_crossover)
+        self.epoch_idx_crossover.append(idx_crossover)
+        self.skf_parent = np.append(self.skf_parent, [[skf[0], skf[1]]], axis = 0)
+        self.skf_parent = np.append(self.skf_parent, [[skf[0], skf[1]]], axis = 0)
+
+        u = np.random.rand(self.dim_uss)    
+        # ~1
+        beta = np.where(u < 0.5, (2*u)**(1/(self.nc +1)), (2 * (1 - u))**(-1 / (1 + self.nc)))
+
+        if skf[0] == skf[1]:
+            #like pa
+            c1 = 0.5*((1 + beta) * pa + (1 - beta) * pb)
+            #like pb
+            c2 = 0.5*((1 - beta) * pa + (1 + beta) * pb)
+
+            c1, c2 = np.clip(c1, 0, 1), np.clip(c2, 0, 1)
+
+            #swap
+            idx = np.where(np.random.rand(len(pa)) < 0.5)[0]
+            c1[idx], c2[idx] = c2[idx], c1[idx]
+        
+        else:
+            #like pa
+            c1 = np.where(idx_crossover, 0.5*((1 + beta) * pa + (1 - beta) * pb), pa)
+            #like pb
+            c2 = np.where(idx_crossover, 0.5*((1 - beta) * pa + (1 + beta) * pb), pa)
+
+            c1, c2 = np.clip(c1, 0, 1), np.clip(c2, 0, 1)
+
+            # NOTE swap
+        return c1, c2
